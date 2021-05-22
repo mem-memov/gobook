849adbe462e6c7a84c4ac657e13a21c071ed24b0
Aleksandr Novikov <mem-memov@yandex.ru>
2021-05-22 11:07:10 +0300

    chan

---
+++ ch8/netcat3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		mustCopy(os.Stdout, conn)
+		fmt.Println("done")
+		done <- struct{}{}
+	}()
+
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+
+	<- done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
